0d39f8abc2a861bee247118f85316a351ff3eae7
arian2004 <arian2004feni@gmail.com>
2025-11-10 02:07:44 +0600

    homepage: About

--- src/pages/Root/Home.jsx
+++ src/pages/Root/Home.jsx
@@ -1,28 +1,13 @@
 import React from "react";
+import Hero from "../../Sections/Home/Hero";
+import About from "../../Sections/Home/About";
 
 const Home = () => {
   return (
-    <header
-      className="hero min-h-screen"
-      style={{
-        backgroundImage:
-          "url(https://i.ibb.co.com/nhtXcP7/image.png)",
-      }}
-    >
-      <div className="hero-overlay"></div>
-      <div className="hero-content text-neutral-content text-center">
-        <div className="max-w-3xl">
-          <h1 className="mb-5 text-7xl font-bold font-heading">
-            Discover the Soul of Bangladesh
-          </h1>
-          <p className="mb-5 text-xl">
-            Find your next adventure — explore hidden destinations, book guided
-            tours, and experience stories from real travelers.
-          </p>
-          <button className="btn btn-primary">Start Exploring</button>
-        </div>
-      </div>
-    </header>
+    <>
+      <Hero />
+      <About />
+    </>
   );
 };
 
