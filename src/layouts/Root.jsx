a0b6ed9fabbf76475f31644a2d0688bd357fa284
arian2004 <arian2004feni@gmail.com>
2025-11-18 12:14:03 +0600

    created a basic navbar

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Outlet } from 'react-router';
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../component/Common/Navbar";
 
 const Root = () => {
   return (
     <>
-     <Outlet /> 
+      <Navbar />
+      <Outlet />
     </>
   );
 };
